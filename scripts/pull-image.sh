96bf1af481b41b64ef78d4fed8e1c0d892b8f7c4
LLLXY <lixinyangemil@outlook.com>
2023-03-02 14:12:58 +0800

    update pull image script

--- scripts/pull-image.sh
+++ scripts/pull-image.sh
@@ -7,18 +7,18 @@ EOF
 sudo systemctl daemon-reload
 sudo systemctl restart docker
 
-sudo docker pull registry.cn-shenzhen.aliyuncs.com/lllxy/corehome-admin:latest
-sudo docker tag registry.cn-shenzhen.aliyuncs.com/lllxy/corehome-admin:latest lixinyang/corehome-admin:latest
+sudo docker pull registry.cn-shenzhen.aliyuncs.com/lllxy/corehome.admin:latest
+sudo docker tag registry.cn-shenzhen.aliyuncs.com/lllxy/corehome.admin:latest lixinyang/corehome.admin:latest
 
-sudo docker pull registry.cn-shenzhen.aliyuncs.com/lllxy/corehome-homepage:latest
-sudo docker tag registry.cn-shenzhen.aliyuncs.com/lllxy/corehome-homepage:latest lixinyang/corehome-homepage:latest
+sudo docker pull registry.cn-shenzhen.aliyuncs.com/lllxy/corehome.homepage:latest
+sudo docker tag registry.cn-shenzhen.aliyuncs.com/lllxy/corehome.homepage:latest lixinyang/corehome.homepage:latest
 
-sudo docker pull registry.cn-shenzhen.aliyuncs.com/lllxy/corehome-reverseproxy:latest
-sudo docker tag registry.cn-shenzhen.aliyuncs.com/lllxy/corehome-reverseproxy:latest lixinyang/corehome-reverseproxy:latest
+sudo docker pull registry.cn-shenzhen.aliyuncs.com/lllxy/corehome.reverseproxy:latest
+sudo docker tag registry.cn-shenzhen.aliyuncs.com/lllxy/corehome.reverseproxy:latest lixinyang/corehome.reverseproxy:latest
 
-sudo docker rmi registry.cn-shenzhen.aliyuncs.com/lllxy/corehome-admin:latest
-sudo docker rmi registry.cn-shenzhen.aliyuncs.com/lllxy/corehome-homepage:latest
-sudo docker rmi registry.cn-shenzhen.aliyuncs.com/lllxy/corehome-reverseproxy:latest
+sudo docker rmi registry.cn-shenzhen.aliyuncs.com/lllxy/corehome.admin:latest
+sudo docker rmi registry.cn-shenzhen.aliyuncs.com/lllxy/corehome.homepage:latest
+sudo docker rmi registry.cn-shenzhen.aliyuncs.com/lllxy/corehome.reverseproxy:latest
 
 sudo docker pull mysql
 
