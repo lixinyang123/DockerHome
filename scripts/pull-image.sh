3f2d8cf4a0e3cd43b309eea4e57c2cbf371cb758
lixinyang123 <lixinyangemil@outlook.com>
2023-03-02 19:01:15 +0800

    init my branch

--- scripts/pull-image.sh
+++ scripts/pull-image.sh
@@ -10,14 +10,14 @@ sudo systemctl restart docker
 sudo docker pull registry.cn-shenzhen.aliyuncs.com/lllxy/corehome.admin:latest
 sudo docker tag registry.cn-shenzhen.aliyuncs.com/lllxy/corehome.admin:latest lixinyang/corehome.admin:latest
 
-sudo docker pull registry.cn-shenzhen.aliyuncs.com/lllxy/corehome.homepage:latest
-sudo docker tag registry.cn-shenzhen.aliyuncs.com/lllxy/corehome.homepage:latest lixinyang/corehome.homepage:latest
+sudo docker pull registry.cn-shenzhen.aliyuncs.com/lllxy/corehome.homepage:home
+sudo docker tag registry.cn-shenzhen.aliyuncs.com/lllxy/corehome.homepage:home lixinyang/corehome.homepage:latest
 
 sudo docker pull registry.cn-shenzhen.aliyuncs.com/lllxy/corehome.reverseproxy:latest
 sudo docker tag registry.cn-shenzhen.aliyuncs.com/lllxy/corehome.reverseproxy:latest lixinyang/corehome.reverseproxy:latest
 
 sudo docker rmi registry.cn-shenzhen.aliyuncs.com/lllxy/corehome.admin:latest
-sudo docker rmi registry.cn-shenzhen.aliyuncs.com/lllxy/corehome.homepage:latest
+sudo docker rmi registry.cn-shenzhen.aliyuncs.com/lllxy/corehome.homepage:home
 sudo docker rmi registry.cn-shenzhen.aliyuncs.com/lllxy/corehome.reverseproxy:latest
 
 sudo docker pull mysql
