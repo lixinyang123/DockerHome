6c2c4e9116a732955a42926354a5cc92140c8d6d
lixinyang123 <lixinyangemil@outlook.com>
2023-09-19 18:37:56 +0800

    Update pull-image.sh

--- scripts/pull-image.sh
+++ scripts/pull-image.sh
@@ -1,12 +1,3 @@
-sudo mkdir -p /etc/docker
-sudo tee /etc/docker/daemon.json <<-'EOF'
-{
-  "registry-mirrors": ["https://xld23qxz.mirror.aliyuncs.com"]
-}
-EOF
-sudo systemctl daemon-reload
-sudo systemctl restart docker
-
 sudo docker pull registry.cn-shenzhen.aliyuncs.com/lllxy/corehome.admin:latest
 sudo docker tag registry.cn-shenzhen.aliyuncs.com/lllxy/corehome.admin:latest lixinyang/corehome.admin:latest
 
