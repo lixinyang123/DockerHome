3260aaaf1de1e5fb8e8ac00e0e96bd3d7f05f693
lixinyang123 <lixinyangemil@outlook.com>
2024-03-23 02:09:41 +0800

    update scripts

--- scripts/pull-image.sh
+++ scripts/pull-image.sh
@@ -4,9 +4,9 @@ docker pull registry.cn-shenzhen.aliyuncs.com/lllxy/corehome.admin:latest
 docker tag registry.cn-shenzhen.aliyuncs.com/lllxy/corehome.admin:latest lixinyang/corehome.admin:latest
 docker rmi registry.cn-shenzhen.aliyuncs.com/lllxy/corehome.admin:latest
 
-docker pull registry.cn-shenzhen.aliyuncs.com/lllxy/corehome.homepage:latest
-docker tag registry.cn-shenzhen.aliyuncs.com/lllxy/corehome.homepage:latest lixinyang/corehome.homepage:latest
-docker rmi registry.cn-shenzhen.aliyuncs.com/lllxy/corehome.homepage:latest
+docker pull registry.cn-shenzhen.aliyuncs.com/lllxy/corehome.homepage:home
+docker tag registry.cn-shenzhen.aliyuncs.com/lllxy/corehome.homepage:home lixinyang/corehome.homepage:latest
+docker rmi registry.cn-shenzhen.aliyuncs.com/lllxy/corehome.homepage:home
 
 docker pull registry.cn-shenzhen.aliyuncs.com/lllxy/corehome.reverseproxy:latest
 docker tag registry.cn-shenzhen.aliyuncs.com/lllxy/corehome.reverseproxy:latest lixinyang/corehome.reverseproxy:latest
